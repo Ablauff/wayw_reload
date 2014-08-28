f48a9f3e58949bb064160177210ca896bfe31e33
pavel <baltika332@gmail.com>
2014-08-28 14:59:33 +0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,61 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Путь Воина:Перезагрузка')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Путь Воина::Перезагрузка")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Главная')
-    end
+    it { should have_content('Путь Воина::Перезагрузка') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Главная') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Помощь')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Путь Воина::Перезагрузка | Помощь")
-    end
+    it { should have_content('Помощь') }
+    it { should have_title(full_title('Помощь')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('О нас')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Путь Воина::Перезагрузка | О нас")
-    end
+    it { should have_content('О нас') }
+    it { should have_title(full_title('О нас')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Контакты')
-    end
-
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("Путь Воина::Перезагрузка | Контакты")
-    end
+    it { should have_content('Контакты') }
+    it { should have_title(full_title('Контакты')) }
   end
-
 end
