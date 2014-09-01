67ab96c99ed356c2f99f77881c8db34bbd871f72
pavel <baltika332@gmail.com>
2014-09-01 12:58:39 +0400

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,52 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Путь Воина::Перезагрузка' }
+    let(:page_title) { '' }
 
-    it { should have_content('Путь Воина::Перезагрузка') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Главная') }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Помощь') }
-    it { should have_title(full_title('Помощь')) }
+    let(:heading)    { 'Помощь' }
+    let(:page_title) { 'Помощь' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('О нас') }
-    it { should have_title(full_title('О нас')) }
+    let(:heading)    { 'О нас' }
+    let(:page_title) { 'О нас' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Контакты') }
-    it { should have_title(full_title('Контакты')) }
+    let(:heading)    { 'Контакты' }
+    let(:page_title) { 'Контакты' }
+    it_should_behave_like "all static pages"
+  end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "О проекте"
+    expect(page).to have_title(full_title('О нас'))
+    click_link "Помощь"
+    expect(page).to have_title(full_title('Помощь'))
+    click_link "Контакты"
+    expect(page).to have_title(full_title('Контакты'))
+    click_link "Главная"
+    click_link "Регистрация!"
+    expect(page).to have_title(full_title('Регистрация'))
+    click_link "Путь Воина"
+    expect(page).to have_title(full_title(''))
   end
 end
